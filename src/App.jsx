c960303d677a3c9cec8471df052ac9cef231df99
Kathleen Santos <kathleen.santos@csacademy.com.br>
2024-05-05 18:47:15 -0300

    search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import BarraDePesquisa from './componentes/BarraDePesquisa'
 import Sidebar from './componentes/Sidebar'
 
 function App() {
   return (
     <>
       <Sidebar/>
+      <BarraDePesquisa />
     </>
   )
 }
---
+++ src/componentes/BarraDePesquisa/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function BarraDePesquisa(){
+    return(
+        <input 
+            className='barra-pesquisa'
+            type="search"
+            placeholder="Digite o que você procura"
+        />
+    )
+}
+
+export default BarraDePesquisa;
